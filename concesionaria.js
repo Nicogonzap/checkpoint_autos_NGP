882d6d3e021462499464130b6a3ae40dc3862975
Nicogonzap <nicolasgonzap@gmail.com>
2021-10-15 19:11:27 -0300

    Agregada funcion puede comprar

--- concesionaria.js
+++ concesionaria.js
@@ -1,4 +1,5 @@
 const autos = require("./autos");
+const personas = require("./personas");
 
 let concesionaria = {
     autos: autos,
@@ -29,10 +30,38 @@ let concesionaria = {
     totalDeVentas: function () {
         let ventas = this.listaDeVentas()
         return ventas.reduce ((curr , next,) => curr + next,0)
+    },
+    puedeComprar: function (auto, persona) {
+        let precioEnCuotas = auto.precio / auto.cuotas
+        if (auto.precio <= persona.capacidadDePagoTotal && precioEnCuotas <= persona.capacidadDePagoEnCuotas) {
+            return true
+        }
+        return false
     }
-    
+
+/*     autosQuePuedeComprar: function(persona){
+        let output = []
+        //this.listaDeVentas().forEach (element => output.push(this.puedeComprar (element,persona)))
+        /*output.push (this.listaDeVentas().forEach (element => {
+            if (this.puedeComprar (element,persona) == true) {
+                output.push (element)
+            }
+        }))
+        let aVender = this.listaDeVentas()
+        aVender.forEach(element {
+            if (this.puedeComprar (element,persona) == true) {
+
+            }
+        })
+        
+        
+        return output 
+    } */
+
 }
 //console.log(concesionaria.autos)
-console.log (concesionaria.listaDeVentas());
-console.log (concesionaria.totalDeVentas());
+//console.log (autos[1]);
+//console.log (personas[0]);
+console.log (concesionaria.autosQuePuedeComprar(personas[0]));
+
 module.exports = concesionaria
---
+++ personas.js
@@ -0,0 +1,16 @@
+const personas = [
+    {
+        nombre: "Juan",
+        capacidadDePagoEnCuotas: 20000,
+        capacidadDePagoTotal: 100000
+    }
+]
+
+/*
+    {
+        nombre: "Roberto",
+        capacidadDePagoEnCuotas: 30000,
+        capacidadDePagoTotal: 200000
+    },
+*/
+module.exports = personas
